525535f2f3a02f120d5d63052c41b0ad0ff106e0
Tomás Rosales <37603053+thomasrosales@users.noreply.github.com>
2020-11-08 10:21:16 -0300

    socketio

--- 01-server/classes/server.ts
+++ 01-server/classes/server.ts
@@ -1,16 +1,37 @@
 import express from 'express';
 import { SERVER_PORT } from '../global/environment';
+import * as socketIO from 'socket.io';
+import http from 'http';
 
 export default class Server {
+  private static _instance: Server;
+
   public app: express.Application;
   public port: number;
+  public io: socketIO.Server;
+  private httpServer: http.Server;
 
-  constructor() {
+  private constructor() {
     this.app = express();
     this.port = SERVER_PORT;
+    this.httpServer = new http.Server(this.app);
+    this.io = require('socket.io')(this.httpServer);
+
+    this.listenToSockets();
+  }
+
+  private listenToSockets() {
+    console.log('Listen Conexion - Scokets');
+    this.io.on('connection', (client) => {
+      console.log('New Client Listening');
+    });
+  }
+
+  public static get instance() {
+    return this._instance || (this._instance = new this());
   }
 
   start(callback: (...args: any[]) => void) {
-    this.app.listen(this.port, callback);
+    this.httpServer.listen(this.port, callback);
   }
 }

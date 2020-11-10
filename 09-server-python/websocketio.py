b3bdb174608dac683a4f56d794357bca9725bd80
Tomás Rosales <37603053+thomasrosales@users.noreply.github.com>
2020-11-09 22:33:24 -0300

    namspace with python and socket.io

--- 09-server-python/websocketio.py
+++ 09-server-python/websocketio.py
@@ -1,17 +1,32 @@
-from flask_socketio import SocketIO, emit, disconnect
+from flask_socketio import SocketIO, emit, disconnect, Namespace
 
 socket_io = SocketIO()
 
 
-@socket_io.on("connect")
-def test_connect():
-    emit("my response", {"data": "Connected"})
+class MessageNameSpace(Namespace):
+    def on_connect(self):
+        print("[ CONNECTED ]")
+
+    def on_disconnect(self):
+        print("[ DISCONNECTED ]")
+        disconnect()
+
+    def on_message(self, payload, callback):
+        print(f"[ EVENT ]: {payload} {callback} ")
+        # emit("my response", {"data": "Connected"})
 
 
-@socket_io.on("disconnect")
-def test_disconnect():
-    print("Client disconnected")
-    disconnect()
+socket_io.on_namespace(MessageNameSpace("/test"))
+
+# @socket_io.on("connect")
+# def test_connect():
+#     emit("my response", {"data": "Connected"})
+
+
+# @socket_io.on("disconnect")
+# def test_disconnect():
+#     print("Client disconnected")
+#     disconnect()
 
 
 def init_socket(app):
